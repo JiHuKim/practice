c1595908789d5ab4c36a6967c57826fcabfdeade
jhkim <rudgusdhr123@naver.com>
2021-08-09 18:06:07 +0900

    Add Main Material UI Styles

--- src/component/main/Main.js
+++ src/component/main/Main.js
@@ -1,15 +1,46 @@
+import { AppBar, Button, IconButton, Tab, Tabs, Toolbar, Typography } from "@material-ui/core";
 import React from "react";
 import useSubmit from "../../customHooks/useSubmit";
+import MenuIcon from '@material-ui/icons/Menu';
+import useMainStyles from "../../customHooks/styles/useMainStyles";
 
-function Main(){
+function Main() {
 
-    const goSignUp = useSubmit({path:"/signUp"});
+    const goSignUp = useSubmit({ path: "/signUp" });
 
-    return(
-        <>
-            <h1>My Web</h1>
-            <input type="button" value="Sign Up" className="cmmBtn" onClick={goSignUp}/>
-        </>
+    const classes = useMainStyles();
+
+    return (
+        <AppBar position="fixed">
+            {/* <Tabs>
+                <Tab icon={<HomeRoundedIcon />} aria-label="Home"/>
+                <Tab label="Sign Up" onClick={goSignUp}/>
+            </Tabs> */}
+            <Toolbar>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    className={classes.menuButton}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography
+                    variant="h6"
+                    className={classes.title}
+                >
+                    Home
+                </Typography>
+                <Button
+                    color="inherit"
+                    onClick={goSignUp}
+                >
+                    Sign Up
+                </Button>
+            </Toolbar>
+
+            {/* <input type="button" value="Sign Up" className="cmmBtn" onClick={goSignUp}/> */}
+        </AppBar>
     );
 
 }
